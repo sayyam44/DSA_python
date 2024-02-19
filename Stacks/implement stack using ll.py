cba852fbb71507c8704bcbcabf1159b5c6cb76fa
TicTacToe <sayyamkundra52@gmail.com>
2024-02-18 23:14:43 -0330

    stack2

---
+++ Stacks/implement stack using ll.py
@@ -0,0 +1,46 @@
+class StackNode:
+ 
+    # Constructor to initialize a node
+    def __init__(self, data=0,next=None):
+        self.data = data
+        self.next = None
+ 
+ 
+class Stack:
+ 
+    # Constructor to initialize the root of linked list
+    def __init__(self):
+        self.head = None
+ 
+    def isEmpty(self):
+        return True if self.head is None else False 
+        
+    def push(self, data):
+        newNode = StackNode(data)
+        newNode.next = self.head #making newnode's next to root because 1stly we need to delete the newnode value 
+        self.head = newNode #root will be pointing towards the topmost value
+        print ("% d pushed to stack" % (data))
+ 
+    def pop(self):
+        if (self.isEmpty()):
+            return float("-inf")
+        temp = self.head
+        self.head = self.head.next #now making the topmost value as the one lower than the actual topmost value
+        popped = temp.data #deleting the topmost value
+        return popped
+ 
+    def peek(self):
+        if self.isEmpty():
+            return float("-inf")
+        return self.head.data #since root is pointing towards the top of the stack.
+ 
+ 
+# Driver code
+stack = Stack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+ 
+print ("% d popped from stack" % (stack.pop()))
+print ("Top element is % d " % (stack.peek()))
+ 
---
+++ Stacks/implement_stack_using_single_queue.py
@@ -0,0 +1,29 @@
+class MyStack:
+
+    def __init__(self):
+        self.q=[]
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        size=len(self.q)
+        for i in range(size): #initially i is pointing o top of the queue.
+            x=self.q.pop() #poping the top value of queue
+            self.q.append(x) #and appending the popped value above the last pushed value
+
+    def pop(self) -> int:
+        if len(self.q)==0:
+            return -1
+        x=self.q.pop()
+        return x
+        
+    def top(self) -> int:
+        if len(self.q)==0:
+            return -1
+        return self.q[-1]
+        
+
+    def empty(self) -> bool:
+        return len(self.q)==0
+        
+
+
