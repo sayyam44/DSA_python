6155d3918e53bb86164f0118700dae1b913d8821
TicTacToe <sayyamkundra52@gmail.com>
2024-02-20 23:59:35 -0330

    new

---
+++ Stacks/minstack.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/min-stack/
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.minstack=[] #in order to min vals till now
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val=min(val,self.minstack[-1] if self.minstack else val)
+        self.minstack.append(val)
+        #appending the min value among the current val and the topmost value in minstack if exists into minstack
+        
+    def pop(self) -> None:
+        self.minstack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.minstack[-1] #min value is always gonna be on the top of minstack
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
---
+++ Stacks/sort stack.py
@@ -0,0 +1,45 @@
+#learn the logic
+#using an extra empty stack 
+#tc=n2,sc=n
+#https://www.geeksforgeeks.org/sort-stack-using-temporary-stack/
+from xml.etree import ElementInclude
+
+
+class Solution:
+    def sorted(self, s):
+        # Code here
+        stk=[]
+        while s :
+            tmp=s.pop() 
+            
+            while stk and stk[-1]>tmp: #we have to push min value into stk
+                tmp2=stk.pop()
+                s.push(tmp2)
+                # stk.pop[-1]
+            stk.push(tmp)
+        return stk
+
+
+#recursive method 
+#https://www.geeksforgeeks.org/sort-a-stack-using-recursion/
+#tc=n2,sc=n
+
+def sortedInsert(element,s):
+    if len(s)==0 or element > s[-1]: #here also we are doing the same as above just here the stk=s 
+        #case 1 - len=0 so s=[9],2nd case element=23 i.e > s[-1] i.e 9 so 
+        # new s=[9,23]
+        s.append(element)
+        return 
+    else: # 3rd case will fall here 
+        temp=s.pop() # temp=23 #remove topmost element
+        sortedInsert(element,s) # this will return s=[9,10] #again call same function after removing 
+        s.append(temp) #s=[9,10,23] #now append the removed value
+
+
+def sortStack(s): #let s=[9,23,10]
+    temp=s.pop()  
+    sortStack(s) #putting all vals in temp and making s empty 
+    sortedInsert(temp,s) #for 1st case s is empty ,temp=9 , 2nd case - temp=23, s=[9],case 3- temp=10,s=[9,23]
+
+
+
