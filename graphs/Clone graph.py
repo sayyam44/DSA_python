c25f3cce4d4d3bf571ff6680496dd04e053f1a43
TicTacToe <sayyamkundra52@gmail.com>
2024-05-16 23:02:59 -0230

    graph2

---
+++ graphs/Clone graph.py
@@ -0,0 +1,25 @@
+## https://leetcode.com/problems/clone-graph/
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        oldtonew={} #creating a hashmap which stores old nodes and new nodes as it is visited 
+
+        def dfs(node): #here we are using dfs approach as this is a recurssion type of problem
+            if node in oldtonew:
+                return oldtonew[node]# for backtracking - if the node is already present in the hashmap then it returns the clone of this node
+
+            #old value is node and new value for that node is copy
+            copy=Node(node.val) #creating a new node 'copy' of the graph as copy if the node is already not present in hashmap
+            oldtonew[node]=copy #in order to add all the nodes into hashmap(old->new)
+
+            for n in node.neighbors: #as we need to create a copy of every neighbor of the current clone node
+                #dfs(n) it returns the copy(clone) we will create for this node 
+                #hence we need to add these copies of the current node to the current node's neighbors list
+                copy.neighbors.append(dfs(n)) #creating all the neighbors of new cloned node 'Copy'
+            return copy
+        return dfs(node) if node else None #inotrder to get a connected graphs
+
---
+++ graphs/Surrounded Regions.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        #we will capture everything except the unsurrounded region eg:the collection of zeroes lying on the border 
+        #we will be moving on the border and we will find if any collection of zeroes that lies on border using dfs and will mark all thes zeroes as 'T'
+        #then do double nested loop to change the  remaining zeroes other than "T" will be converted to x as required 
+        #dfs
+        
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS 
+                or board[r][c] != "O"):
+                return
+            board[r][c] = "T"
+            dfs(r + 1, c) #now checking in all directions i.e. finding a collection of 0's on border
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        ROWS, COLS = len(board), len(board[0])
+        # 1. (DFS) Capture unsurrounded regions (O -> T)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (board[r][c] == "O" and
+                    (r in [0, ROWS - 1] or c in [0, COLS - 1])): #here we are checking the border zeroes only
+                    dfs(r, c)
+        
+        # 2. Capture surrounded regions (O -> X)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                    
+        # 3. Uncapture unsurrounded regions (T -> O)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+
