83b4401919d0a8a22545cbcf26a1d811e765b122
TicTacToe <sayyamkundra52@gmail.com>
2024-06-03 23:42:50 -0230

    graph_3_june

---
+++ graphs/Floyd Warshall Algorithm.py
@@ -0,0 +1,40 @@
+#tc=n3
+V = 4
+INF = 99999
+def floydWarshall(graph):
+	dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
+	for k in range(V):#k loop will be used to perform i,j iteration for allthe vertices that is from 1 to 4
+
+		for i in range(V):
+			for j in range(V): #i,j loop will be used for a single matrix
+				dist[i][j] = min(dist[i][j],
+								dist[i][k] + dist[k][j]
+								)
+	printSolution(dist)
+
+def printSolution(dist):
+	print ("Following matrix shows the shortest distances\
+between every pair of vertices")
+	for i in range(V):
+		for j in range(V):
+			if(dist[i][j] == INF):
+				print ("%7s" % ("INF"),end=" ")
+			else:
+				print ("%7d\t" % (dist[i][j]),end=' ')
+			if j == V-1:
+				print ()
+"""
+			10
+	(0)------->(3)
+		|		 /|\
+	5 |		 |
+		|		 | 1
+	\|/		 |
+	(1)------->(2)
+			3		 """
+graph = [[0, 5, INF, 10],
+		[INF, 0, 3, INF],
+		[INF, INF, 0, 1],
+		[INF, INF, INF, 0]
+		]
+floydWarshall(graph)
