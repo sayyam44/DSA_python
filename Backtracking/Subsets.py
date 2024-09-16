ed68dc1cee770fff55648089eccc939649d90bc8
TicTacToe <sayyamkundra52@gmail.com>
2024-09-15 23:04:36 -0230

    15th september

--- Backtracking/Subsets.py
+++ Backtracking/Subsets.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/subsets/
+#TC=2^N (as for each element we have 2 possibilities that is either take it or not take it)
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
