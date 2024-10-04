a1e4dfaa5ac7945ce9c164a6e559b427359ec0de
TicTacToe <sayyamkundra52@gmail.com>
2024-10-04 12:01:26 -0230

    4th oct

---
+++ Backtracking/Non Decreasing Subsequences.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/non-decreasing-subsequences/
+def main(nums):
+    res=set()
+    def backtrack(ind,path_till_now):
+        if len(path_till_now)>=2:
+            res.add(tuple(path_till_now))
+        for i in range(ind,len(nums)):
+            if not path_till_now or path_till_now[-1]<=nums[i]:
+                backtrack(i+1,path_till_now+[nums[i]])
+
+    backtrack(0,[])
+    return list(res)
+
+print(main([4, 6, 7, 7]))
