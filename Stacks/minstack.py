c8e211bbdde470ef6fa60ab7750d86606702fe54
TicTacToe <sayyamkundra52@gmail.com>
2024-11-16 22:58:18 -0330

    16thNov

--- Stacks/minstack.py
+++ Stacks/minstack.py
@@ -1,3 +1,4 @@
+# Updated
 class MinStack:
 
     def __init__(self):
--- Stacks/valid_parentheses.py
+++ Stacks/valid_parentheses.py
@@ -1,35 +1,17 @@
+# updated
 # Input: s = "()[]{}"
 # Output: true
 #tc=n,sc=n
 class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
-        closetoopen={")":"(","]":"[","}":"{"} #keys are closing and vals are opening brackets
-        
-        for c in s:
-            if c not in closetoopen: #if c is not any closing bracket
-                stack.append(c)
-                continue
-            if not stack or stack[-1]!=closetoopen[c]: #the corresponding value of c in close to open must be same as stack's top value
-                return 0
-            stack.pop()
-        return not stack #RETURN TRUE IF STACK IS EMPTY
-
-#method is same as above but this is bit easier
-class Solution:
-    def isValid(self, s: str) -> bool:
-        open_list = ["[","{","("]
-        close_list = ["]","}",")"]
-        stack=[]
-        for i in s:
-            if i in open_list:
-                stack.append(i)
-            elif i in close_list:
-                if not stack: 
-                    return 0
-                pos=close_list.index(i)
-                if (stack.pop()!=open_list[pos]):
-                    return 0
+        paren={')':'(','}':'{',']':'['}
+        for p in s:
+            if p not in paren:
+                stack.append(p)
             else:
-                return 0
-        return not stack 
+                if stack and stack[-1]==paren[p]:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
