5b27d7d234f40a72601eaaacbb22a2fda9f33f1a
TicTacToe <sayyamkundra52@gmail.com>
2024-11-28 21:21:28 -0330

    28thNov

--- Binary Trees/Bottom view of Binary Tree.py
+++ Binary Trees/Bottom view of Binary Tree.py
@@ -7,6 +7,51 @@
 #and also on the right side draw vertical lines with +1,+2,...
 #and we need to find the last elements of all these vertical lines.
 
+
+#Method-1 DFS (using level and hd both)
+class Node:
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+
+def bottomview_dfs(root):
+    def dfs(node, hd, level):
+        if not node:
+            return
+        
+        # If the hd is not in the map, or we find a node at the same hd but deeper, update the map
+        if hd not in m or level >= m[hd][1]:
+            m[hd] = (node.data, level)
+        
+        # Traverse left and right with updated hd and level
+        dfs(node.left, hd - 1, level + 1)
+        dfs(node.right, hd + 1, level + 1)
+    
+    # Map to store (value, level) for each horizontal distance (hd)
+    m = {}
+    
+    dfs(root, 0, 0)
+    
+    # Traverse the map in sorted order of horizontal distances and print values
+    for hd in sorted(m.keys()):
+        print(m[hd][0], end=" ")
+
+if __name__ == '__main__':
+    root = Node(20)
+    root.left = Node(8)
+    root.right = Node(22)
+    root.left.left = Node(5)
+    root.left.right = Node(3)
+    root.right.left = Node(4)
+    root.right.right = Node(25)
+    root.left.right.left = Node(10)
+    root.left.right.right = Node(14)
+    
+    print("Bottom view of the given binary tree:")
+    bottomview_dfs(root)
+
+
 #append all the values in form of node,hd(hosrizontal distance) into 
 # the queue and all its child nodes along
 # with left's child's level -1,-2,-3 and also add its right 
@@ -15,8 +60,6 @@
 #now pop from the queue and append one by one the values into the dict in hd,node format and if some 
 #other element comes while traversing the queue with same hd we need to update it like in level order traversal
 #but vertically.
-
-# IN THIS CASE WE WILL FOLLOOW DFS FORMAT
 
 from collections import deque
 class Node:
--- Binary Trees/Top View.py
+++ Binary Trees/Top View.py
@@ -3,7 +3,51 @@
 
 # PLEASE SEE BOTTOM VIEW BEFORE THIS
 
-# IN THIS CASE WE WILL FOLLOOW DFS FORMAT
+#Method-1 using DFS
+class Node:
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+
+def topview_dfs(root):
+    def dfs(node, hd, level):
+        if not node:
+            return
+        
+        # If the hd is not in the map, or we find a node at the same hd but shallower, update the map
+        if hd not in m or level < m[hd][1]:
+            m[hd] = (node.data, level)
+        
+        # Traverse left and right with updated hd and level
+        dfs(node.left, hd - 1, level + 1)
+        dfs(node.right, hd + 1, level + 1)
+    
+    m = {}
+    
+    # Start DFS from the root
+    dfs(root, 0, 0)
+    
+    # Traverse the map in sorted order of horizontal distances and print values
+    for hd in sorted(m.keys()):
+        print(m[hd][0], end=" ")
+
+if __name__ == '__main__':
+    root = Node(20)
+    root.left = Node(8)
+    root.right = Node(22)
+    root.left.left = Node(5)
+    root.left.right = Node(3)
+    root.right.left = Node(4)
+    root.right.right = Node(25)
+    root.left.right.left = Node(10)
+    root.left.right.right = Node(14)
+    
+    print("Top view of the given binary tree:")
+    topview_dfs(root)
+
+
+#Method-2 BFS
 from collections import deque
 class Node:
     def __init__(self,key):
