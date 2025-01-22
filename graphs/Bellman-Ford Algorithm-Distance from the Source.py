ef3dbab1d2a2ea9d7683cb02a3231527958a048d
TicTacToe <sayyamkundra52@gmail.com>
2025-01-22 20:27:22 -0330

    dsa_jan

--- graphs/Bellman-Ford Algorithm-Distance from the Source.py
+++ graphs/Bellman-Ford Algorithm-Distance from the Source.py
@@ -1,9 +1,10 @@
+# updated
 #Bellman ford algorithm 
 #This algorithm is also used to find the shortest distance from source till every other node 
 #The only difference b/w bellman ford and dijkistra's algorithm is that this algorithm can 
 #be used in graph with negative weights i.e. for a graph with negative cycle.
 
-#Negative cycle means a graph with sum of its weights as negative value.
+#Negative cycle means a graph with sum of its weights of the current path as negative value.
 
 #Bellman ford only works in the case of a directed graph
 
@@ -20,40 +21,43 @@
 #then we can say it contains a negative cycle.
 
 class Graph:
+    def __init__(self,vertices):
+        self.V=vertices
+        self.graph=[]
+    def addEdge(self,u,v,w):
+        self.graph.append((u,v,w))
 
-    def __init__(self, vertices):
-        self.V = vertices  
-        self.graph = []
-    def addEdge(self, u, v, w):
-        self.graph.append([u, v, w])
-    def printArr(self, dist):
-        print("Vertex Distance from Source")
+    def BellmanFord(self,start):
+        dist=[float("inf")]*(self.V)
+        dist[start]=0
+
+        for _ in range(self.V-1):
+            for parent,child,weight in self.graph:
+                if dist[parent]!=float("inf") and dist[parent]+weight<dist[child]:
+                    dist[child]=dist[parent]+weight
+            
+        for parent,child,weight in self.graph: #negative cycle
+                if dist[parent]!=float("inf") and dist[parent]+weight<dist[child]:
+                    print('NEGATIVE WEIGHT')
+                    return None
+
+        self.print_solution(dist)
+
+    def print_solution(self,dist):
         for i in range(self.V):
-            print("{0}\t\t{1}".format(i, dist[i]))
-    def BellmanFord(self, src):
-        dist = [float("Inf")] * self.V
-        dist[src] = 0
-        for _ in range(self.V - 1):
-            for u, v, w in self.graph:
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
-        #here we are doing nth relaxation to check negative cycle
-        #and if we find some lower distance now at nth iteration then that is wrong 
-        for u, v, w in self.graph:
-            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                print("Graph contains negative weight cycle")
-                return
-        self.printArr(dist)
+            print(f"{i}\t\t{dist[i]}")
 
-if __name__ == '__main__':
-    g = Graph(5)
-    g.addEdge(0, 1, -1)
-    g.addEdge(0, 2, 4)
-    g.addEdge(1, 2, 3)
-    g.addEdge(1, 3, 2)
-    g.addEdge(1, 4, 2)
-    g.addEdge(3, 2, 5)
-    g.addEdge(3, 1, 1)
-    g.addEdge(4, 3, -3)
 
-    g.BellmanFord(0)
+
+g = Graph(5)
+g.addEdge(0, 1, -1)
+g.addEdge(0, 2, 4)
+g.addEdge(1, 2, 3)
+g.addEdge(1, 3, 2)
+g.addEdge(1, 4, 2)
+g.addEdge(3, 2, 5)
+g.addEdge(3, 1, 1)
+g.addEdge(4, 3, -3)
+
+g.BellmanFord(0)
+
--- graphs/Cheapest Flights within K stops.py
+++ graphs/Cheapest Flights within K stops.py
@@ -1,4 +1,42 @@
-# https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
+# updated
+#BELLMAN FORD ALGORITHM
+# https://leetcode.com/problems/cheapest-flights-within-k-stops/
+#BELLMAN FORD ALGORITHM 
+#tc=E*K
+#Bellman ford algo 
+
+class Solution:
+    def findCheapestPrice(self,n, flights, src, dst, k):
+        prices=[float("inf")]*n
+        prices[src]=0
+        
+        for i in range(k+1): #k means number of stops so 1stly do for k=0 that is no stop then do for k+1=1 that is 1 stop at max
+            temp=prices[:]
+            
+            for src,dst,price in flights: #here it is doing bfs s#s=source,d=destination,p=price (weight)
+                if prices[src]!=float("inf"):# we only relax an edge (src->dst) if we have already found a valid cost to reach node src.
+                    temp[dst]=min(temp[dst],prices[src]+price)
+
+            # Update prices after processing all edges in this iteration
+            prices=temp
+        return prices[dst] if prices[dst] != float('inf') else -1
+
+solution = Solution()
+
+# Example graph: 3 cities (0, 1, 2), with edges (src, dst, price)
+n = 3
+flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500]]
+src = 0
+dst = 2
+k = 1
+
+# Find the cheapest price from src to dst with at most k stops
+result = solution.findCheapestPrice(n, flights, src, dst, k)
+print(f"Cheapest price: {result}")  
+
+
+
+#USING DIJKISTRA'S Algorithm 
 from collections import defaultdict, deque
 import math
 
@@ -28,3 +66,4 @@ src = 0
 dst = 3
 k = 1
 print(findCheapestPrice(n, flights, src, dst, k))  
+
