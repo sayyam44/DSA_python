0aa894f821ce82be8c60f218f47aaa4149cf813c
TicTacToe <sayyamkundra52@gmail.com>
2025-01-30 22:07:15 -0330

    dsa

--- graphs/Word Search.py
+++ graphs/Word Search.py
@@ -1,6 +1,5 @@
-# updated
 # https://leetcode.com/problems/word-search/description/
-
+#This method is without using any extra scpace of visited set
 #TC=n*m*dfs 
 #dfs runs len(word) times everytime\
 #and dfs is cased 4 times everytime 
@@ -31,6 +30,35 @@ class Solution:
                     return True
         return False
         
+        
+#TC=n*m*dfs 
+#dfs runs len(word) times everytime\
+#and dfs is cased 4 times everytime 
+#Therefore TC=n*m*4^n
+# Space Complexity: O(L) (Depth of recursion stack + visited set)
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS=len(board)
+        COLS=len(board[0])
+        visited=set()
+        def dfs(i,j,k):
+            if k==len(word):
+                return True
+            if i < 0 or i >= ROWS or j < 0 or j >= COLS or (i, j) in visited or board[i][j] != word[k]: 
+                return False
+            visited.add((i,j))
+            found=(dfs(i+1,j,k+1) or dfs(i-1,j,k+1) or dfs(i,j+1,k+1) or dfs(i,j-1,k+1))
+            visited.remove((i,j))
+            return found
+
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j]==word[0] and dfs(i,j,0):
+                    return True
+        return False
+
+
 
 
             
