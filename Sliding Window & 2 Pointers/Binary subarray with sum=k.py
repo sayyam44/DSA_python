649831c6eec61c7ceb039646319a2b95f8efd9e1
TicTacToe <sayyamkundra52@gmail.com>
2025-02-24 22:01:41 -0330

    dsa

--- Sliding Window & 2 Pointers/Binary subarray with sum=k.py
+++ Sliding Window & 2 Pointers/Binary subarray with sum=k.py
@@ -1,4 +1,3 @@
-#count subarrays with sum=k
 # https://leetcode.com/problems/binary-subarrays-with-sum/
 
 #brute force approach
@@ -20,50 +19,21 @@ class Solution:
 #1st i am counting the number of subarrays with sum<=k
 #step1-if sum is < goal then add all the possible subarrays with the subarray
 #length of (r-l+1)
-#if at anypoint wew find the sum>goal then make the sum in control by 
+#if at anypoint we find the sum>goal then make the sum in control by 
 # subtratcing value at l index from sum and moving l ahead 
-class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        if goal<0:
-            return 0
-        l,r=0,0
-        cnt=0
-        add=0
-        while r<len(nums):
-            sum+=nums[r]
-            if sum>goal:
-                while sum>goal:
-                    sum-=nums[l]
-                    l+=1
-            elif sum<=goal:
-                cnt+=(r-l+1) #important thing to notice as we are adding all
-                #the subarrays now from r till l
-            r+=1
-        return cnt
-
-
-#now to get the subarrays with sum == k we will subtract the 
-#function(nums,goal) - function(nums,goal-1)
-#function(nums,goal) => It will give all the subarrays with sum <=goal
-#function(nums,goal-1) => It will give all the subarrays with sum <=goal-1
-#subtraction of this will give us the required no. of subarrays with = goal.
-# tc=2x2n , sc=1
-
 class Solution(object):
-    def new_func(self,nums,goal):
-        if goal<0:
+    def atMost(self, nums, goal):
+        if goal < 0:
             return 0
-        l,r=0,0
-        cnt=0
-        add=0
-        while r<len(nums):
-            add+=nums[r]
-            while add>goal:
-                add-=nums[l]
-                l+=1
-            cnt+=(r-l+1)#important thing to notice as we are adding all
-                #the subarrays now from r till l
-            r+=1
+        l, curr_sum, cnt = 0, 0, 0
+        for r in range(len(nums)):
+            curr_sum += nums[r]
+            while curr_sum > goal:
+                curr_sum -= nums[l]
+                l += 1
+            cnt += (r - l + 1)  # Counts all subarrays with sum ≤ goal
         return cnt
-    def numSubarraysWithSum(self, nums, goal):
-        return self.new_func(nums,goal)-self.new_func(nums,goal-1)
+
+    def numSubarraysWithSum(self, nums, goal): 
+        # Subarrays with sum exactly goal=Subarrays with sum ≤ goal−Subarrays with sum ≤ goal - 1
+        return self.atMost(nums, goal) - self.atMost(nums, goal - 1)
